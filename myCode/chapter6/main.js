b3beb4b9db82d4c2cc726a9489b6deb0401826b0
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-05 14:27:56 +0200

    Conditionals
    IF Statement, While and For Loop.

--- myCode/chapter6/main.js
+++ myCode/chapter6/main.js
@@ -1,5 +1,27 @@
-let myName = "Lindokuhle Yende";
+let myName = "LY";
+let sheepCounted = 0
+let myFamily = ["Siphosami", "Thando", "Thandeka"]
+
+
 
 if (myName.length > 5){
     console.log("Hello "+ myName)
+} else if (myName.length == 2){
+    console.log("Try your fullname")
+} else{
+    console.log("Add your initials")
+};
+
+// while (sheepCounted < 10){
+    // console.log("I have counted " + sheepCounted + " sheep!");
+    // sheepCounted++;
+// };
+// console.log("Zzzzzzzzzzz");
+
+for (var i=0; i<3; i++){
+    console.log("I have counted " + i + " sheep!");
+};
+
+for (var i=0; i< myFamily.length; i++){
+    console.log("This is my family member " + myFamily[i]);
 };
