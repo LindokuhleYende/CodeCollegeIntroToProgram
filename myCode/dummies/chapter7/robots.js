bbe8397e525f9b97219d757fc1232a431761c391
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-07 15:08:55 +0200

    Updating the style of the robot using Javascript

---
+++ myCode/dummies/chapter7/robots.js
@@ -0,0 +1,13 @@
+document.getElementById("lefteye").style.backgroundColor = "purple"
+document.getElementById("head").style.transform = "rotate(15deg)"
+// Put yellow dots around his right eye.
+document.getElementById("righteye").style.border =
+"4px yellow dotted";
+// Change his left arm’s color.
+document.getElementById("leftarm").style.backgroundColor =
+"#FF00FF";
+// Change the text color.
+document.getElementById("body").style.color = "#FF0000";
+// Give Douglas hair.
+document.getElementById("head").style.borderTop =
+"5px black solid";
