e9a9105cb3074aaf369050e9c163cf15f8cecbf6
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-08 13:20:35 +0200

    Animating the robot

--- myCode/dummies/chapter7/robots.js
+++ myCode/dummies/chapter7/robots.js
@@ -11,3 +11,45 @@ document.getElementById("body").style.color = "#FF0000";
 // Give Douglas hair.
 document.getElementById("head").style.borderTop =
 "5px black solid";
+document.getElementById("mouth").style.backgroundColor = "brown";
+
+
+
+var rightEye = document.getElementById("righteye");
+var body = document.getElementById("body");
+var leftEye = document.getElementById("lefteye");
+
+
+rightEye.addEventListener("click", moveUpDown);
+leftEye.addEventListener("click", moveUpDown);
+body.addEventListener("click", moveRightLeft)
+
+function moveUpDown(e){
+    var robotPart = e.target;
+    var top = 0;
+    var id = setInterval(frame, 10) // draw every 10ms
+
+    function frame() {
+        robotPart.style.top = top + "%";
+        top++;
+        if (top === 20){
+            clearInterval(id);
+}
+}
+}
+
+var leftArm = document.getElementById("leftarm");
+leftArm.addEventListener("click", moveRightLeft);
+function moveRightLeft(e) {
+    var robotPart = e.target;
+    var id = setInterval(frame, 5) // draw every 5ms
+    var left = 15;
+    robotPart.style.left = left + "%";
+    function frame() {
+        if (left === 40){
+            left++;
+            clearInterval(id);
+}
+}
+}
+
