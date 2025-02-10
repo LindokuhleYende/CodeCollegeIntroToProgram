c2c516d3d2fd69ee79df886e9b4500419a5e4c0b
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-10 17:36:04 +0200

    Updated html of Chapter10 and Chapter9

--- myCode/dummies/chapter8/car.js
+++ myCode/dummies/chapter8/car.js
@@ -1,22 +1,16 @@
-var favorite ={
-    "hobby":"Content Creation",
-    "Passion":"Data Analytics",
-    "Place":"Cafe",
-    "Friends": 2
-};
-console.log(favorite)
+
 var dreamCar = {
     model: "Suv",
     make: "Oldsmobile",
-    year: 1980,
-    color: "brown",
+    year: 2000,
+    color: "pink",
     price: 4500,
     bodyStyle: "Luxury Car",
     carName :"Maserati"
 };
 
 
-document.getElementById("pricetag").innerHTML = dreamCar.price;
+document.getElementById("pricetag").innerHTML = dreamCar["price"];
 document.getElementById("modelyear").innerHTML = dreamCar.year;
 document.getElementById("body").innerHTML = dreamCar.carName + " " + dreamCar.model;
 document.getElementById("body").style.background = dreamCar.color;
