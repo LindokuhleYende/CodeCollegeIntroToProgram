b34cc5c136e8d9c07697aeffe11d6bd4fbef77e4
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-11 10:25:41 +0200

    Updated the styles.css and main.js
    Added some styling and created new variables.

--- myCode/dummies/chapter11/main.js
+++ myCode/dummies/chapter11/main.js
@@ -1,4 +1,11 @@
 var friends = ["Busi", "Hope", "Katlego"];
 var parents = ["Mother", "Father"];
 
-var family = [friends, parents]
+var family = [friends, parents];
+console.log(family);
+
+var pet1 = "Dog";
+var pet2 = "Chicken";
+var pet3 = "Rabit";
+
+var myAnimals = [pet1, pet2, pet3];
