745b6cab34a4c51d0c309b5c24f8ef0a042cd454
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-11 12:19:36 +0200

    working with array methods

--- myCode/dummies/chapter11/main.js
+++ myCode/dummies/chapter11/main.js
@@ -10,3 +10,14 @@ var pet3 = "Rabit";
 
 var myAnimals = [pet1, pet2, pet3];
 console.log(family.concat(myAnimals));
+
+myAnimals.unshift("Cat");
+console.log(myAnimals);
+console.log(myAnimals.pop());
+
+console.log(friends.slice(1,3));
+
+let longArray = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+let shortArr = longArray.slice(3,5);
+console.log(shortArr)
