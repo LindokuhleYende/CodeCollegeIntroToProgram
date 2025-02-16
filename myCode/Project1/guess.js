8b4dccfe17ce58422d34014cb0df195b34d1d4ff
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-16 23:43:46 +0200

    Update guess.js
    Created an if statement to display correct or incorrect guess. Added a while loop for when guess is not equal to secret num. And an if statement inside the while loop to give an idea of the guess num

--- myCode/Project1/guess.js
+++ myCode/Project1/guess.js
@@ -2,3 +2,19 @@ let secret = 7;
 let answer = prompt("Please guess the secret number between (1-20)");
 // Convert the string guess to an integer so that we can compare
 var guess = parseInt (answer);
+
+if (secret === guess){
+    alert("Correct Guess!");
+
+}else{
+    alert("Sorry incorrect Guess!")
+}
+
+while (guess !== secret){
+    if (guess<7){
+        alert("Incorrect, too low!");
+    }else{
+        alert("Incorrect, too high!");
+    }
+    break
+}
