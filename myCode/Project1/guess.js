38f47af2bd88786831ba41f7249e5bfac942f032
Lindokuhle <yendelindokuhle2@gmail.com>
2025-02-16 23:31:24 +0200

    Update guess.js

---
+++ myCode/Project1/guess.js
@@ -0,0 +1,4 @@
+let secret = 7;
+let answer = prompt("Please guess the secret number between (1-20)");
+// Convert the string guess to an integer so that we can compare
+var guess = parseInt (answer);
